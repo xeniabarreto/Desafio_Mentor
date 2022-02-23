628bc819348fd684cdefef60d1849f724e82cf0d
Xênia Barreto <xeniabarreto22@gmail.com>
2022-02-23 02:24:36 -0300

    inserindo anotações For dentro For

--- desafios/starcase.js
+++ desafios/starcase.js
@@ -24,16 +24,20 @@ staircase();
 // SOLUÇÃO DOIS
 let n = 6;
 
+//Ele faz o for I uma vez, depois o lopping completo do J e depois volta pro proximo I e refaz todo processo ate a condição ser falsa
 for ( let i = 1; i <= n; i++) {
     for ( let j = 1; j <= n; j++){
         if ( i + j > n){
+            //write faz ele imprimir a # sem seguir pra linha de baixo
             process.stdout.write("#");
         }
         else {
+            //write faz ele imprimir um espaço sem seguir pra linha de baixo
             process.stdout.write(" ");
         }        
     }
-    console.log("");
+    // quando termina o laco I e J, imprime uma string vazia pulando passando pra linha de baixo
+    console.log(""); 
 }
 //}
 //staircaseTwo();
