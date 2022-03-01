060eab997c28b88a4223d2ad9210b537af72540a
Xênia Barreto <xeniabarreto22@gmail.com>
2022-03-01 17:35:01 -0300

    update atv. leitura

---
+++ desafios/aula02/reverseArray.js
@@ -0,0 +1,21 @@
+//Exemplo do link de leitura:
+//https://ricardo-reis.medium.com/empilhar-stack-em-arrays-javascript-9504645d18ff
+
+
+function reverse(str) {
+    let stack = [];
+    //adicionar letras no stack (pilha)
+    for (let i = 0; i < str.length; i++) {
+        stack.push(str[i]);
+    }
+
+    //excluir letra do stack
+    let reverseStr = '';
+    while (stack.length > 0) {
+        reverseStr += stack.pop();
+    }
+
+    return reverseStr;
+}
+
+console.log(reverse('0 1 2 3 4 5 6 7 8 9 '));
