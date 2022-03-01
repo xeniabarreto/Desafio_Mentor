4d2b50927b3dac177e72dec5425d0b6b6918bf54
Xênia Barreto <xeniabarreto22@gmail.com>
2022-03-01 20:02:46 -0300

    solução exercícuo

---
+++ desafios/aula02/reverseArray.js
@@ -0,0 +1,30 @@
+
+/*
+arr = [1,4,3,2]
+console.log(arr)
+arr.reverse()
+console.log(arr)*/
+
+// Desafio
+https://www.hackerrank.com/challenges/arrays-ds/problem?isFullScreen=true
+
+reverseArray = () => {
+    
+    let array = [1,4,3,2];
+
+    for (let i = 1; i < array.length; i++){
+        let previousNumber = array[i-1];
+        let currentNumber = array[i]
+
+        if(previousNumber != 0 && currentNumber != 0){
+            array.splice(i, 0 , " ")
+            i++
+        }
+    }
+
+    array.reverse()    
+    return array.join('')
+
+}
+
+console.log(reverseArray())
