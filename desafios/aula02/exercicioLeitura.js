cc29bf78dd82053f33fbe243476b6ebd3278210b
Xênia Barreto <xeniabarreto22@gmail.com>
2022-03-01 18:24:58 -0300

    update link

--- desafios/aula02/exercicioLeitura.js
+++ desafios/aula02/exercicioLeitura.js
@@ -19,3 +19,22 @@ function reverse(str) {
 }
 
 console.log(reverse('0 1 2 3 4 5 6 7 8 9 '));
+
+let listaGatos = ['madruga','neguinho', 'ponta branca'];
+console.log(listaGatos)
+
+
+listaGatos.push('Branquinha');
+console.log(listaGatos)
+
+listaGatos.push('Nega', 'Oncinha');
+console.log(listaGatos)
+
+console.log(listaGatos.length);
+
+
+listaGatos.pop()
+console.log(listaGatos)
+
+listaGatos.splice(3,1)
+console.log(listaGatos)
