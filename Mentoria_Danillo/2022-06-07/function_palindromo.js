e57c3f70a315bff07650a545046c8db9d852d985
Xenia Barreto <xeniabarreto22@gmail.com>
2022-06-07 23:09:07 -0300

    updates

---
+++ Mentoria_Danillo/2022-06-07/function_palindromo.js
@@ -0,0 +1,26 @@
+// Palíndromo é uma palavra, frase ou número que permanece igual quando lida de trás para diante.
+// Por extensão, palíndromo é qualquer série de elementos com simetria linear, ou seja,
+// que apresenta a mesma sequência de unidades nos dois sentidos.
+
+function palindromo(palavra) {
+    JSON.parse(palavra)
+    //transformar string num array
+    //fazer for para verificar cada letra da palavra
+    //ler palavra de frente para traz
+    //ler a palavra de traz para frente
+    //usar condicionais IF/ELSE
+    //se as palavras forem diferentes false
+    //se iguais retorna true
+  
+    palavra.algumaFuncao();
+  
+    return true;
+  }
+  
+  // console.log(palindromo("radar"));
+  // console.log(palindromo("palavra"));
+  // console.log(palindromo("ovo"));
+  // console.log(palindromo("omissíssimo"));
+  // console.log(palindromo("mamam"));
+  
+  ["r", "a", "d", "a", "r"];
