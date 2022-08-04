1061139884e27212f7864bc4b0cf496b10151e62
Xênia Barreto <xenia.barreto@queimadiaria.com.br>
2022-08-04 11:51:35 -0300

    add activities with recursão

--- Mentoria_Danillo/2022-07-12/fulano_case.js
+++ Mentoria_Danillo/2022-07-12/fulano_case.js
@@ -2,15 +2,46 @@
 function fulano(palavra) {
     let string = palavra;
     let array = string.split('');
-    let includes = array.push(' ');
 
-    for (let i = 0; i <= array.length+4 ; i++){
-        
+    let tamanhoOriginal = array.length // mantendo o tamanho array, mesmo depois de passar o split
+    
+            console.log(palavra)
+
+    for (let i = 0; i <= tamanhoOriginal; i++){
+
+    
         let indice = array.lastIndexOf();
-        let remove = array.splice(indice);
+        array.splice(indice);
         console.log(array.join(""));
     }
-} 
 
+
+} 
 console.log(fulano("FULANO"));
+
+
+
+//exemplo RECURSÃO
+
+function fulano(palavra, controlador) {
+    if (controlador === 2) {
+      console.log(palavra);
+    } else {
+      fulano(palavra, controlador + 1);
+    }
+  }
   
+  fulano("QUALQUER COISA", 0);
+
+
+  //solucão COM RECURSÃO - quando chama uma a função dentro dela mesmo.
+
+  function fulano(palavra) {
+    if (palavra !== "") console.log(palavra);
+  
+    if (palavra.length > 0) {
+      fulano(palavra.slice(0, palavra.length - 1));
+    }
+  }
+  
+  fulano("JABUTICABA");
---
+++ Mentoria_Danillo/2022-08-03/function_recursão.js
@@ -0,0 +1,5 @@
+// Palíndromo é uma palavra, frase ou número que permanece igual quando lida de trás para diante.
+// Por extensão, palíndromo é qualquer série de elementos com simetria linear, ou seja,
+// que apresenta a mesma sequência de unidades nos dois sentidos.
+
+//CRIAR UMA FUNÇÃO COM RECURSÃO
