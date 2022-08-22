aefd0129706088f2e6e2807dfd97e9bb9feee25a
Xênia Barreto <xenia.barreto@queimadiaria.com.br>
2022-08-21 23:14:14 -0300

    add exercises duplicates

--- Mentoria_Danillo/2022-07-12/fulano_case.js
+++ Mentoria_Danillo/2022-07-12/fulano_case.js
@@ -1,4 +1,4 @@
-
+/*
 function fulano(palavra) {
     let string = palavra;
     let array = string.split('');
@@ -45,3 +45,79 @@ function fulano(palavra, controlador) {
   }
   
   fulano("JABUTICABA");
+*/
+/*
+
+  let array = [1,2,3,4,5,6,7,8,9,10]; 
+  
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    console.log(element);
+  }
+*/
+/*
+function imprimeArray(palavra){
+  let qualquerCoisa = palavra.split('')
+  
+
+  
+  console.log(qualquerCoisa[0])
+  qualquerCoisa - 1
+
+
+
+
+}
+console.log(imprimeArray("bola"))
+ 
+
+
+function teste(palavra){
+  let qualquerCoisa = palavra.split('')
+  console.log(qualquerCoisa);
+  teste(qualquerCoisa+1);
+}
+
+console.log(teste("BOLA"))
+
+*/
+
+
+
+
+//Exemplo
+
+function somatorio(x){
+ if(x<=1)
+  return 1;
+
+ else
+  return x + somatorio(x-1) ;
+}
+console.log(somatorio(1))
+
+
+
+function test(array, controle) {
+  if(controle <= array.length - 1) {
+    console.log(array[controle])
+
+    test(array, controle + 1)
+  }
+}
+
+test([2, 3, 4], 0);
+
+
+
+function test2(array) {
+  if(array.length !== 0) {
+    console.log(array[0])
+
+    test2(array.slice(1, array.length))
+  }
+}
+
+test2([2, 3, 4]);
+
+// recursão sempre com validação/ condicional que defina fim do looping
--- Mentoria_Danillo/2022-08-03/function_recursão.js
+++ Mentoria_Danillo/2022-08-03/function_recursão.js
@@ -3,3 +3,48 @@
 // que apresenta a mesma sequência de unidades nos dois sentidos.
 
 //CRIAR UMA FUNÇÃO COM RECURSÃO
+
+
+function palindromo(palavra){
+  for (let i = 0; i < palavra.length; i++) {
+      let frente = palavra[i]
+      let tras = palavra[palavra.length-1 -i]
+
+      if(frente != tras){
+          return false
+      }
+      return true //depois que acaba o for e nao retornou false, colocamos o true
+  }
+}
+//console.log(palindromo("radar"));
+
+
+
+function isPalindrome2(word) {
+  let numOfLetters = word.length,
+    lastCharIndex = numOfLetters - 1;
+
+  if (numOfLetters === 0 || numOfLetters === 1) {
+    return true;
+  }
+
+  if (word[0] === word[lastCharIndex]) {
+    word = word.substr(0, lastCharIndex).substr(1); //substr retorna uma parte da string
+    return isPalindrome2(word);
+  }
+
+  return false;
+}
+
+console.log(isPalindrome2("radar"))
+console.log(isPalindrome2("amor"))
+
+
+/**
+ * const str = 'Mozilla';
+console.log(str.substr(1, 2));
+// expected output: "oz"
+
+console.log(str.substr(2));
+// expected output: "zilla"
+ */
